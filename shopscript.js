ea8d0d57d8a1bdfe1e3251008537033cacbf8c5a
ama6543 <amanda.j.slettli@gmail.com>
2026-01-19 12:46:34 +0100

    funskjoner til cart

--- shopscript.js
+++ shopscript.js
@@ -1,6 +1,13 @@
-document.getElementById("cart-button").addEventListener("click", function(){
-    document.getElementById("cart").classList.toggle("hidden")
+document.getElementById("cart-button").addEventListener("click", ()=> {
+    handleVogn("cart")
 })
+document.getElementById("cart-btn2").addEventListener("click", ()=> {
+    handleVogn("cart")
+})
+//ekstern funskjon til handlevogn
+function handleVogn(id){
+    document.getElementById(id).classList.toggle("hidden")
+}
 
 //funksjon for produktopplistning:
 function fetchProducts(){
@@ -10,16 +17,46 @@ function fetchProducts(){
                 <a href="#">${p.category}</a>
                 <h3>${p.title}</h3>
                 <p>kr. ${p.price},-</p>
+                <p>Storage: ${p.storage}</p>
                 <button onClick="addToCart(${p.prodid})">Legg til handlevogn</button>
             </article>`)
             document.getElementById("product-list").innerHTML = productHTML
 }
+//Slik at chart viser riktig opplysninger
 fetchProducts()
-
 //Legg til i handlevogn
 function addToCart(prodid){
-    console.log("legg til produkt med id:" + prodid)
-    cart.push(prodid)
-    console.log(cart)
+    //når p.storage er null kan ikke blitt adda
+    let print = "<p>Din Handlevogn</p>"
+    let total = 0
+    products.forEach((p)=>{
+        if(p.storage >= 1){
+            if(p.prodid == prodid){
+                //hvis storage har 1 eller flere produkter kan den bli lagt inn i cart
+                console.log("legg til produkt med id:" + prodid)
+                cart.push(prodid)
+                document.getElementById("cart-quantity").innerHTML = cart.length
+                p.storage--
+            }
+        }
+        if(cart.includes(p.prodid)){
+                let each = cart.filter((id) => id === p.prodid).length
+                print += `<tr><td>${p.title}</td><td>${p.price}, -</td><td>x${each}</td><td class="delete"><button onclick=deleCart(${p.prodid})>X</button></td>`
+                total += Number(p.price) * each 
+            }
+        if(p.storage <= 0){
+            p.storage = 0
+        }
+    })
+     document.getElementById("cart").innerHTML = `<table>${print}<tr><td>Totalt: kr ${total},-</td></tr></table>`
+     fetchProducts()
+    }
+//fjerne ifra cart
+function deleCart(prodid){
+    let index = cart.indexOf(prodid)
+    console.log("fjern produkt med id" + index)
+    cart.splice(index, 1)
+    addToCart(prodid)
+    //oppdatere statues til cart antall
     document.getElementById("cart-quantity").innerHTML = cart.length
 }
